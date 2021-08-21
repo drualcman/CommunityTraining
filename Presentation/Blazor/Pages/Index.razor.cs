02f1c3a95df15be7a59b16f31a18fec717635bae
Sergi Ortiz Gómez <drualcman@msn.com>
2021-08-21 17:50:17 +0800

    first version for the web.

---
+++ Presentation/Blazor/Pages/Index.razor.cs
@@ -0,0 +1,80 @@
+﻿using CommunityTraining.Blazor.Shared;
+using CommunityTraining.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CommunityTraining.Blazor.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        [Inject]
+        public HttpClient ApiClient { get; set; }
+
+        private YoutubePlayer Reproductor = new YoutubePlayer();
+        public List<PlayList> ListaReproduccion;
+        public string UserName = "Gest";
+        public string Video;
+        private bool Clicked;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadList();
+        }
+
+        public async Task LoadList()
+        {
+            HttpResponseMessage responseMessage = await ApiClient.GetAsync("playlist");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                ListaReproduccion = await responseMessage.Content.ReadFromJsonAsync<List<PlayList>>();
+            }
+            else ListaReproduccion = new List<PlayList>();
+        }
+
+        public async Task AddVideo()
+        {
+            if (!Clicked)
+            {
+                Clicked = true;
+                PlayList video = await Reproductor.AddToPlayListAsync(Video);
+                video.Ownner = UserName;
+                HttpResponseMessage responseMessage = await ApiClient.PostAsJsonAsync("playlist", video);
+                if (responseMessage.IsSuccessStatusCode) ListaReproduccion.Add(video);
+                else await JsRuntime.InvokeVoidAsync("alert", "No se ha podido agregar el video");
+                Clicked = false;
+            }
+        }
+
+        public async Task DeleteVideo(string id)
+        {
+            if (!Clicked)
+            {
+                Clicked = true;
+                HttpResponseMessage responseMessage = await ApiClient.DeleteAsync($"playlist/{id}");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    PlayList video = ListaReproduccion.Where(v => v.Id == id).FirstOrDefault();
+                    if (!ListaReproduccion.Remove(video)) await JsRuntime.InvokeVoidAsync("location.reload");
+                }
+                else await JsRuntime.InvokeVoidAsync("alert", "No se ha podido borrar el video");
+                Clicked = false;
+            }
+        }
+
+        string Playing = "qeMFqkcPYcg";
+
+        void PlayVideo()
+        {
+            Playing = Helpers.Video.ExtraerId(Video);
+        }
+    }
+}
---
+++ Presentation/Blazor/Shared/YoutubePlayer.razor.cs
@@ -0,0 +1,110 @@
+﻿using CommunityTraining.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunityTraining.Blazor.Shared
+{
+    public partial class YoutubePlayer: IDisposable
+    {
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        [Parameter]
+        public string PlayerId { get; set; }
+
+        [Parameter]
+        public string VideoId { get; set; }
+
+
+        private static Action actionApi;
+        private static Action actionPlayer;
+
+        bool IsApiLoad = false;
+        bool IsPlayerReady = false;
+
+        protected override void OnInitialized()
+        {
+            actionApi = ApiIsLoaded;
+            actionPlayer = PlayerIsReady;
+        }
+
+        private async void ApiIsLoaded()
+        {
+            Console.WriteLine("ApiIsLoaded");
+            IsApiLoad = true;
+            await JsRuntime.InvokeVoidAsync("youtubeApi.loadIFramePlayer");
+            StateHasChanged();
+        }
+
+        private async void PlayerIsReady()
+        {
+            Console.WriteLine("PlayerIsReady");
+
+            if (IsPlayerReady)
+            {
+                if (VideoId is not null)
+                    await JsRuntime.InvokeVoidAsync("youtubeApi.payVideo", VideoId);
+            }
+            else
+            {
+                IsPlayerReady = true;
+            }
+            StateHasChanged();
+        }
+
+        [JSInvokable]
+        public static void ApiLoaded()
+        {
+            Console.WriteLine("api loated");
+            actionApi.Invoke();
+        }
+
+        [JSInvokable]
+        public static void PlayerReady()
+        {
+            Console.WriteLine("player ready");
+            actionPlayer.Invoke();
+        }
+
+        public Task<PlayList> AddToPlayListAsync(string Video)
+        {
+            string id;
+            string title;
+            id = Helpers.Video.ExtraerId(Video);
+
+            title = id;
+
+            PlayList playVideo = new PlayList
+            {
+                Id = id,
+                Title = title,
+                Url = Video
+            };
+            return Task.FromResult(playVideo);
+        }        
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JsRuntime.InvokeVoidAsync("youtubeApi.loadIFramePlayer");
+            }
+        }
+
+        async void PlayVideo()
+        {
+            await JsRuntime.InvokeVoidAsync("youtubeApi.payVideo", VideoId);
+        }
+
+        void IDisposable.Dispose()
+        {
+            IsApiLoad = false;
+            IsPlayerReady = false;
+            JsRuntime.InvokeVoidAsync("youtubeApi.dispose");
+        }
+    }
+}

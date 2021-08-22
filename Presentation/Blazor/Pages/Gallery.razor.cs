b31f55074d81488cb1ff6c5928d7f4d571dceace
Sergi Ortiz Gómez <drualcman@msn.com>
2021-08-22 18:06:43 +0800

    fix layout according to the actual community page

---
+++ Presentation/Blazor/Pages/Gallery.razor.cs
@@ -0,0 +1,74 @@
+﻿using CommunityTraining.Blazor.Shared;
+using CommunityTraining.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CommunityTraining.Blazor.Pages
+{
+    public partial class Gallery
+    {
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        [Inject]
+        public HttpClient ApiClient { get; set; }
+
+        [Inject]
+        public NavigationManager Navigaton { get; set; }
+
+        protected YoutubePlayer Reproductor = new YoutubePlayer();
+        public List<PlayList> ListaReproduccion;
+        private bool Clicked;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadList();
+        }
+
+        public async Task LoadList()
+        {
+            ListaReproduccion = await ApiClient.GetFromJsonAsync<List<PlayList>>("playlist");
+        }
+
+        public async Task DeleteVideo(string id)
+        {
+            if (!Clicked)
+            {
+                Clicked = true;
+                HttpResponseMessage responseMessage = await ApiClient.DeleteAsync($"playlist/{id}");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    PlayList video = ListaReproduccion.Where(v => v.Id == id).FirstOrDefault();
+                    if (!ListaReproduccion.Remove(video)) await JsRuntime.InvokeVoidAsync("location.reload");
+                }
+                else await JsRuntime.InvokeVoidAsync("alert", "No se ha podido borrar el video");
+                Clicked = false;
+            }
+        }
+
+        string Playing = "qeMFqkcPYcg";
+        bool ShowVideo;
+        void PlayVideo(string Video)
+        {
+            Playing = Helpers.Video.ExtraerId(Video);
+            Reproductor.PlayVideo();
+            ShowVideo = true;
+        }
+        void EditVideo(string id)
+        {
+            Navigaton.NavigateTo($"video/{id}");
+        }
+
+        void Cerrar()
+        {
+            ShowVideo = false;
+            Reproductor.StopVideo();
+        }
+    }
+}
--- Presentation/Blazor/Pages/Index.razor.cs
+++ Presentation/Blazor/Pages/Index.razor.cs
@@ -13,62 +13,6 @@ namespace CommunityTraining.Blazor.Pages
 {
     public partial class Index
     {
-        [Inject]
-        public IJSRuntime JsRuntime { get; set; }
-
-        [Inject]
-        public HttpClient ApiClient { get; set; }
-
-        [Inject]
-        public NavigationManager Navigaton { get; set; }
-
-        protected YoutubePlayer Reproductor = new YoutubePlayer();
-        public List<PlayList> ListaReproduccion;
-        private bool Clicked;
-
-        protected override async Task OnInitializedAsync()
-        {
-            await LoadList();
-        }
-
-        public async Task LoadList()
-        {
-            ListaReproduccion = await ApiClient.GetFromJsonAsync<List<PlayList>>("playlist");            
-        }
-
-        public async Task DeleteVideo(string id)
-        {
-            if (!Clicked)
-            {
-                Clicked = true;
-                HttpResponseMessage responseMessage = await ApiClient.DeleteAsync($"playlist/{id}");
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    PlayList video = ListaReproduccion.Where(v => v.Id == id).FirstOrDefault();
-                    if (!ListaReproduccion.Remove(video)) await JsRuntime.InvokeVoidAsync("location.reload");
-                }
-                else await JsRuntime.InvokeVoidAsync("alert", "No se ha podido borrar el video");
-                Clicked = false;
-            }
-        }
-
-        string Playing = "qeMFqkcPYcg";
-        bool ShowVideo;
-        void PlayVideo(string Video)
-        {
-            Playing = Helpers.Video.ExtraerId(Video);
-            Reproductor.PlayVideo();
-            ShowVideo = true;
-        }
-        void EditVideo(string id)
-        {
-            Navigaton.NavigateTo($"video/{id}");
-        }
-
-        void Cerrar()
-        {
-            ShowVideo = false;
-            Reproductor.StopVideo();
-        }
+       
     }
 }
